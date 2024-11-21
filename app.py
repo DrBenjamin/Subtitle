90f33f69739d1050ecf5506352a6765b794c2517
DrBenjamin <drdrbenjamin@icloud.com>
2024-11-21 13:10:57 +0100

    testing

--- app.py
+++ app.py
@@ -11,10 +11,12 @@ if uploaded_files:
     try:
         # Converting example.srt into output.vtt
         srt_to_vtt(path_to_my_srt_file, path_to_converted_vtt_file)
+        with open('output.vtt', 'r') as file:
+            content = file.read()
         st.success("Datei(en) erfolgreich konvertiert.")
         st.download_button(
             label="Download Untertitel",
-            data=path_to_converted_vtt_file,
+            data=content,
             file_name="Untertitel.vtt",
             mime="text/vtt",
         )
