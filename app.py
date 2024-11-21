871288d1aed6e1cfc9cb8221e7ae631085a4868b
DrBenjamin <drdrbenjamin@icloud.com>
2024-11-21 15:02:02 +0100

    stable

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
 import streamlit as st
 import io
-from srt_to_vtt import srt_to_vtt
 
+def srt_to_vtt(srt_file):
+    buffer_vtt = io.StringIO()
+    buffer_vtt.write("WEBVTT\n\n")
+    for line in srt_file:
+        if line.strip().isdigit():
+            continue
+        if "-->" in line:
+            line = line.replace(",", ".")
+        buffer_vtt.write(line)
+    return buffer_vtt
+            
 st.title("Untertitel Konverter")
 st.write("Dieses Tool konvertiert `srt` Untertitel Dateien in das `vtt` Format.")
 uploaded_file = st.file_uploader("Datei hochladen", accept_multiple_files=False, type='srt')
 if uploaded_file:
     try:
-        buffer = io.StringIO()
-        srt_to_vtt(uploaded_file.read(), buffer)
-        content = buffer.getvalue()
+        buffer = srt_to_vtt(io.StringIO(uploaded_file.read().decode("utf-8")))
         st.success("Datei(en) erfolgreich konvertiert.")
         st.download_button(
             label="Download Untertitel",
-            data=content,
+            data=buffer.getvalue(),
             file_name="Untertitel.vtt",
             mime="text/vtt",
         )
