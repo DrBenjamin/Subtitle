07a85e2a616fd74dc7ff685dc963c346899b789a
DrBenjamin <drdrbenjamin@icloud.com>
2024-11-21 13:04:06 +0100

    minor changes

--- app.py
+++ app.py
@@ -9,11 +9,10 @@ st.title("Untertitel Konverter")
 uploaded_files = st.file_uploader("Datei(en) hochladen", accept_multiple_files=True, type='srt')
 if uploaded_files:
     try:
-        st.success("Datei(en) erfolgreich hochgeladen.")
+        # Converting example.srt into output.vtt
+        srt_to_vtt(path_to_my_srt_file, path_to_converted_vtt_file)
+        st.success("Datei(en) erfolgreich konvertiert.")
+        st.download_button(path_to_converted_vtt_file)
+
     except S3Error as e:
         st.error(f"Error: {e}")
-        
-# Converting example.srt into output.vtt
-vtt_file = srt_to_vtt(path_to_my_srt_file, path_to_converted_vtt_file)
-
-st.download_button(vtt_file, "Download VTT-Datei", "Click here to download your file", "vtt")
